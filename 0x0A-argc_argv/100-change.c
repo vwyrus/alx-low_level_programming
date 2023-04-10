f85474a08753b5452441ae08f14299351b4fa680
vwyrus <oputuogheneyoma@gmail.com>
2023-04-10 13:28:27 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,13 +3,10 @@
 #include "main.h"
 
 /**
-
  * main - prints the minimum number of coins to
  * make change for an amount of money
  * @argc: number of arguments
  * @argv: array of arguments
- *
-
  * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
